8beec73c0232686ab75cf2f233135de20a35325f
KatsiarynaBarsukova <tulovo@tut.by>
2022-10-22 19:30:33 +0300

    Классная работа лекция 5-6

---
+++ src/main/java/services/SimpleDriver.java
@@ -0,0 +1,15 @@
+package services;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleDriver {
+    
+    public WebDriver getDriver(){
+
+         System.setProperty("webdriver.chrome.driver",
+                 "C:\\Users\\sony\\IdeaProjects\\TAF_21_1\\src\\test\\resources\\chromedriver.exe");
+
+         return new ChromeDriver();
+    }
+}
