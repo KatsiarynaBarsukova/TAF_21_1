88897c803ea1580386e2cfa3f5b8fdaa1bcd078a
KatsiarynaBarsukova <tulovo@tut.by>
2022-10-23 11:03:55 +0300

    Классная работа лекция 8

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import baseEntities.BaseTest;
+import configuration.ReadProperties;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+    @Test
+    public void successLoginTest() {
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.loginSuccessful(ReadProperties.username(), ReadProperties.password());
+
+        Assert.assertTrue(new DashboardPage(driver).isPageOpened());
+    }
+
+    @Test
+    public void incorrectUsernameTest() {
+        // Какие-то действия
+
+        DashboardPage dashboardPage = new DashboardPage(driver);
+    }
+
+    @Test
+    public void incorrectPswTest() {
+
+    }
+}
